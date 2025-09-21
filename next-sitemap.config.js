fe5d5c18985fc2ecaaa340612d1e733b1cd74987
mohammed junaid <mohammedjunaidtech1@gmail.com>
2025-09-21 09:19:40 +0000

    404 Page Not Found Sorry, we couldn’t find the page you’re looking for.

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,8 +2,5 @@
 module.exports = {
   siteUrl: 'https://www.toolzenweb.com',
   generateRobotsTxt: true,
-  // The default file name for the sitemap index is sitemap.xml
-  // No need to specify sitemapName unless you want something different.
-  // The library will automatically generate sitemap-0.xml, sitemap-1.xml, etc.
-  // and reference them in the main sitemap.xml index.
+  outDir: './public',
 };
