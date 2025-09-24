8e70a67a1b8530913c4ae3117b5bf6ec1b4fdb49
mohammed junaid <mohammedjunaidtech1@gmail.com>
2025-09-24 07:49:37 +0000

    dont remove hidden ads place let it be as they are for future

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -86,7 +86,7 @@ export default function BlogIndexPage() {
                         </ul>
                     </CardContent>
                 </Card>
-
+                
             </aside>
         </div>
       </main>
