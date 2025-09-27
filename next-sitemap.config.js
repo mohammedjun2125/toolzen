2baaaed18ebe204c21b43571033ace1a109bc5a7
mohammed junaid <mohammedjunaidtech1@gmail.com>
2025-09-27 22:17:06 +0000

    but according to sitemap-0.xml <?xml version="1.0" encoding="UTF-8"?> <u

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,57 +1,21 @@
 
 /** @type {import('next-sitemap').IConfig} */
-
-// Dynamically import from ESM modules
-const getToolsAndCategories = async () => {
-    const { tools, categories } = await import('./src/lib/tools.ts');
-    return { tools, categories };
-};
-
-const getBlogPosts = async () => {
-    const { mockPosts } = await import('./src/lib/blog.ts');
-    return mockPosts;
-}
-
 module.exports = {
   siteUrl: 'https://www.toolzenweb.com',
   generateRobotsTxt: true,
   outDir: './out',
-  exclude: ['/products/*'], // This will exclude any pages under /products/
-  
-  additionalPaths: async (config) => {
-    const { tools, categories } = await getToolsAndCategories();
-    const posts = await getBlogPosts();
-    
-    const toolPaths = tools.map(tool => ({
-      loc: `/tools/${tool.id}`,
-      changefreq: 'weekly',
-      priority: 0.9,
-      lastmod: new Date().toISOString(),
-    }));
-
-    const categoryPaths = categories.map(category => ({
-      loc: `/category/${category.id}`,
-      changefreq: 'weekly',
-      priority: 0.8,
-      lastmod: new Date().toISOString(),
-    }));
-    
-    const blogPaths = posts.map(post => ({
-      loc: `/blog/${post.slug}`,
-      changefreq: 'monthly',
-      priority: 0.7,
-      lastmod: new Date(post.date).toISOString(),
-    }));
-
-    return [...toolPaths, ...categoryPaths, ...blogPaths];
-  },
-
+  // Exclude any paths that are not meant to be in the sitemap.
+  // This is a failsafe, but the primary mechanism will be the build process.
+  exclude: ['/products/*', '/products'],
   robotsTxtOptions: {
     policies: [
       {
         userAgent: '*',
         allow: '/',
       },
+      // You can add more specific rules here if needed in the future
+      // For example, to disallow crawling of a specific directory:
+      // { userAgent: '*', disallow: '/private/' },
     ],
     additionalSitemaps: [
       'https://www.toolzenweb.com/sitemap.xml',
