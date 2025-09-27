00d2e7d291a80262c8b25a3802f9bb977247c779
mohammed junaid <mohammedjunaidtech1@gmail.com>
2025-09-27 22:18:24 +0000

    done ?

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -5,17 +5,13 @@ module.exports = {
   generateRobotsTxt: true,
   outDir: './out',
   // Exclude any paths that are not meant to be in the sitemap.
-  // This is a failsafe, but the primary mechanism will be the build process.
-  exclude: ['/products/*', '/products'],
+  exclude: ['/products', '/products/*'],
   robotsTxtOptions: {
     policies: [
       {
         userAgent: '*',
         allow: '/',
       },
-      // You can add more specific rules here if needed in the future
-      // For example, to disallow crawling of a specific directory:
-      // { userAgent: '*', disallow: '/private/' },
     ],
     additionalSitemaps: [
       'https://www.toolzenweb.com/sitemap.xml',
