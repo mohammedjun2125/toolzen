26c1eb6271ea59f676184d4ffb6d8151b61b0063
mohammed junaid <mohammedjunaidtech1@gmail.com>
2025-10-03 22:53:12 +0000

    Couldn't verify your site We couldn't verify your site. Make sure the ch

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -87,6 +87,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
        <head>
+        <meta name="google-adsense-account" content="ca-pub-8015189558686269" />
         <Script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8015189558686269"
