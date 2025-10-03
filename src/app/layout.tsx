83d2b4c2511b76550abe4505011128c7e7dc9631
mohammed junaid <mohammedjunaidtech1@gmail.com>
2025-10-03 20:49:28 +0000

    For quicker ad setup and delivery, we recommend inserting these header s

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -87,6 +87,13 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
        <head>
+        <script async src="//www.ezojs.com/ezoic/sa.min.js"></script>
+        <script>
+            {`
+              window.ezstandalone = window.ezstandalone || {};
+              ezstandalone.cmd = ezstandalone.cmd || [];
+            `}
+        </script>
         <script src="https://cmp.gatekeeperconsent.com/min.js" data-cfasync="false" async></script>
         <script src="https://the.gatekeeperconsent.com/cmp.min.js" data-cfasync="false" async></script>
         <Script
