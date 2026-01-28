493a5754707e95eaf216965e3423932c256dcab6
mohammed junaid <mohammedjunaidtech1@gmail.com>
2026-01-28 17:55:22 +0000

    add this new google adsence code as given Verify site ownership
    To get

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -109,6 +109,8 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
        <head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9367491431332157"
+     crossOrigin="anonymous"></script>
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(websiteSchema) }}
