d8b506a62260ddf31565fab18ae82b89ec99c90b
mohammed junaid <mohammedjunaidtech1@gmail.com>
2026-02-04 15:20:39 +0000

    Fix redirect loop and align application with Vercel domain configuration

--- next.config.js
+++ next.config.js
@@ -29,21 +29,6 @@ const nextConfig = {
       },
     ],
   },
-  async redirects() {
-    return [
-      {
-        source: '/:path*',
-        has: [
-          {
-            type: 'host',
-            value: 'www.toolzenweb.com',
-          },
-        ],
-        destination: 'https://toolzenweb.com/:path*',
-        permanent: true,
-      },
-    ]
-  },
   async rewrites() {
     return [
       {
